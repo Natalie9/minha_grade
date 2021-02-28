e9c3474712a5b619ddcd6426d7833345fa8d9502
natalie9 <natalietolentino9@gmail.com>
2021-02-27 21:23:35 -0300

    first commit 🎉

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title>
+          Minha Grade
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+  </q-layout>
+</template>
+<script>
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout'
+})
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-layout>
+    <div class="fluxo">
+      <Mermaid :nodes="materias" type="graph LR"></Mermaid>
+    </div>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import Mermaid from 'components/Mermaid'
+import Materias from 'assets/materias.json'
+
+export default defineComponent({
+  name: 'PageIndex',
+  components: {
+    Mermaid
+  },
+  data () {
+    return {
+      materias: Materias.data
+    }
+  },
+  methods: {
+    editNode (nodeId) {
+      console.log({ nodeId })
+    }
+  }
+})
+</script>
+<style>
+.fluxo {
+  height: 100vh;
+  background: black;
+  width: 100vw;
+}
+</style>
